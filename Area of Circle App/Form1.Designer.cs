006dd109a4466dfebe4fa18cea5edf9d88c16870
Abdirizak Mohamed <halac122@gmail.com>
2022-05-15 19:38:53 +0300

    Add project files.

---
+++ Area of Circle App/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Area_of_Circle_App
+{
+    partial class frmDemo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 319);
+            this.Name = "frmDemo";
+            this.Text = "Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Area of Circle App/Form1.cs
@@ -0,0 +1,10 @@
+namespace Area_of_Circle_App
+{
+    public partial class frmDemo : Form
+    {
+        public frmDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
